1cadb231d74a5e1a915f7ebf429924f28e6c986c
lae1965 <lae1965@yandex.ru>
2021-08-29 19:25:05 +0300

    Сделал домашнее задание к уроку №1

---
+++ lesson1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Message from './Message'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                I just have made my first React.js application!!!
+                <Message sign="Andrey Lashkevich"></Message>
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ lesson1/src/Message.js
@@ -0,0 +1,9 @@
+import './Message.css';
+
+function Message({sign}) {
+    return (
+        <p className="message">{sign}</p>
+    );
+}
+
+export default Message;
